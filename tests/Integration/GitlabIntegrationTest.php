5e41578f6d7f573ff392396e358c5d210ca3fb63
Eirik S. Morland <eirik@morland.no>
2021-05-22 21:56:59 +0200

    Add gitlab integration test

---
+++ tests/Integration/GitlabIntegrationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Violinist\NeedsUpdateCheckRunner\Tests\Integration;
+
+use Violinist\UpdateCheckData\UpdateCheckData;
+
+class GitlabIntegrationTest extends IntegrationBase
+{
+    public function testGithubEmptyKnown()
+    {
+        $url = getenv('PRIVATE_REPO_GITLAB');
+        $token = getenv('PRIVATE_USER_TOKEN_GITLAB');
+        $data = new UpdateCheckData();
+        $result = $this->runContainer([
+            'project_url' => $url,
+            'user_token' => $token,
+            'update_check_data' => sprintf("'%s'", json_encode(serialize($data))),
+        ]);
+        $result_found = false;
+        foreach ($result as $item) {
+            if (empty($item->context->type)) {
+                continue;
+            }
+            if ($item->context->type !== 'result') {
+                continue;
+            }
+            $data = unserialize($item->context->data);
+            if (!$data instanceof UpdateCheckData) {
+                continue;
+            }
+            self::assertNotEmpty($data->getLastSha()->getSha());
+            $result_found = true;
+        }
+        self::assertEquals(true, $result_found);
+    }
+}
